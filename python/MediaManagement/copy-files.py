8bd196f1afa60c41f933be47e776996e71da851b
Derek Leeth <derek.leeth@gmail.com>
2019-12-28 16:16:28 -0500

    MediaManagement/copy-files: create

---
+++ python/MediaManagement/copy-files.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""copy-files.py
+
+Copy files to external storage.
+"""
+
+import sys
+import os
+import subprocess
+import argparse
+
+
+def Main(source, destination, excludedirsfilepath, excludedirsfilepath):
+    '''
+    robocopy $Source $Destination /MAXAGE:30 /R:30 /W:10 /dcopy:DAT /Z /S /log:$RoboCopyLog
+    '''
+
+    cmd = [
+            'Robocopy.exe',
+            os.path.abspath(source),
+            os.path.abspath(destination)
+        ]
+    
+    copyProcess = subprocess.Popen(copyCommand)
+
+
+def _cli():
+    parser = argparse.ArgumentParser(
+            description=__doc__,
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+            argument_default=argparse.SUPPRESS)
+    parser.add_argument('-s', '--source', help="This is the foo argument")
+    parser.add_argument('-d', '--destination', help="This is the bar argument")
+    parser.add_argument('-ed', '--excludedirsfilepath', default="", help="This is the bar argument")
+    qux_help = ("This argument will show its default in the help due to "
+                "ArgumentDefaultsHelpFormatter")
+    # parser.add_argument('-q', '--qux', default=3, help=qux_help)
+    args = parser.parse_args()
+    return vars(args)
+
+
+if __name__ == '__main__':
+    main(**_cli())
